ffee59bd31ac2de1ca6830fba94e6a6a52948853
MattMatheus <MattMatheus@users.noreply.github.com>
2022-06-08 07:48:26 -0700

    Create Get-VirtualMachineSize.ps1

---
+++ Get-VirtualMachineSize.ps1
@@ -0,0 +1,27 @@
+function Get-VirtualMachineSize {
+    
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]    
+        [string]$VmName,
+        [string]$VmSize
+    )
+    
+    try {
+        Get-AzTenant -ErrorAction STOP
+    } catch {
+        Connect-AzAccount -ErrorAction STOP
+    }
+
+    $ExitArray = New-Item -TypeName "System.Collections.ArrayList"
+
+    $server = Get-AzVM -Name $VmName
+    if($Server.VmSize -eq $VmSize) {
+        $ExitArray.Append($server.name)
+    } 
+    
+    return $ExitArray
+}
+
+Export-ModuleMember -Function Get-VirtualMachineSize
+
